0628f7a542c3568550f7da68d9ba91b4dc87e906
justaprogrammer321 <architbhrgv@gmail.com>
2024-02-11 18:59:19 +0530

    projectidadded

--- src/components/firebase/firebaseInit.js
+++ src/components/firebase/firebaseInit.js
@@ -12,7 +12,7 @@ const firebaseConfig = {
     apiKey: import.meta.env.VITE_APP_API_KEY,
     authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
     databaseURL: import.meta.env.VITE_APP_DATABASE_URL,
-    projectId: import.meta.env.VITE_APP_PROJECT_ID,
+    projectId: import.meta.env.VITE_APP_PROJECT_ID || saavyas-23,
     storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
     messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
     appId: import.meta.env.VITE_APP_APP_ID,
